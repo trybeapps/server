d3cdc1c96a2ddecc67a00bbbd7ffeeffbc30436a
Nirmal Almara <rkumarnirmal@gmail.com>
2016-12-03 18:30:27 +0530

    Upload + style tweaks

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask, g, session, abort, redirect, url_for, render_template, request, escape
+from flask import Flask, g, session, abort, redirect, url_for, render_template, request, escape, send_from_directory
+from werkzeug.utils import secure_filename
 from functools import wraps
 from flask_sqlalchemy import SQLAlchemy
 import hashlib
 import os
 import bcrypt
 
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+UPLOAD_FOLDER = os.path.join(APP_ROOT, 'uploads')
+ALLOWED_EXTENSIONS = set(['pdf'])
+
 app = Flask(__name__)
+
+# set the upload path
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# set the secret key.  keep this really secret:
+app.secret_key = 'ff29b42f8d7d5cbefd272eab3eba6ec8'
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/libreread_dev'
 db = SQLAlchemy(app)
 
@@ -43,7 +55,6 @@ def login_required(f):
 @app.route('/')
 def index():
     if 'email' in session:
-        print (session)
         return render_template('home.html')
     return render_template('landing.html')
 
@@ -100,5 +111,38 @@ def logout():
     session.pop('email', None)
     return redirect(url_for('index'))
 
-# set the secret key.  keep this really secret:
-app.secret_key = 'ff29b42f8d7d5cbefd272eab3eba6ec8'
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
+@app.route('/book-upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            print ('Book uploaded successfully!')
+            return redirect(url_for('index'))
+
+@app.route('/b/<filename>')
+def send_book(filename):
+    return send_from_directory('uploads', filename)
+
+@app.route('/b/cover/<filename>')
+def send_book_cover(filename):
+    return send_from_directory('uploads/images', filename)
