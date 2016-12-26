ee97b64347043aebed3c87f0c1ddcd3bb0006ea1
Nirmal Almara <rkumarnirmal@gmail.com>
2016-12-26 19:24:04 +0530

    confirm_email working

--- app/auth/controllers.py
+++ app/auth/controllers.py
@@ -33,16 +33,15 @@ def confirm_email(token):
     try:
         email = confirm_token(token)
     except:
-        flash('The confirmation link is invalid or has expired.', 'danger')
+        return 'The confirmation link is invalid or has expired.'
     user = User.query.filter_by(email=email).first_or_404()
     if user.confirmed:
-        flash('Account already confirmed. Please login.', 'success')
+        return 'Account already confirmed. Please login.'
     else:
         user.confirmed = True
-        user.confirmed_on = datetime.datetime.now()
         db.session.add(user)
         db.session.commit()
-        flash('You have confirmed your account. Thanks!', 'success')
+        return 'You have confirmed your account. Thanks!'
     return redirect(url_for('main.home'))
 
 
