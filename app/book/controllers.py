fd46cc1c5820f5a512e65ea84be3c82a07b9d383
Nirmal Almara <rkumarnirmal@gmail.com>
2017-01-02 13:48:23 +0530

    init attachment bug fix

--- app/book/controllers.py
+++ app/book/controllers.py
@@ -25,15 +25,32 @@ def upload_file():
     import json
 
     payload = json.dumps({
-      'settings': {
-        'number_of_shards': 4,
-        'number_of_replicas': 0
-      }
+        'description': 'Process documents',
+        'processors': [
+            {
+                'attachment': {
+                    'field': 'thedata',
+                    'indexed_chars': -1
+                }
+            },
+            {
+                'set': {
+                    'field': 'attachment.title',
+                    'value': '{{ title }}'
+                }
+            },
+            {
+                'set': {
+                    'field': 'attachment.page',
+                    'value': '{{ page }}'
+                }
+            }
+        ]
     })
 
     print payload
 
-    r = requests.put('http://elasticsearch:9200/lr_index', data=payload)
+    r = requests.put('http://elasticsearch:9200/_ingest/pipeline/attachment', data=payload)
 
     print r.text
 
