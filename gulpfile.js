bc51e1fbeed87ebc839116d5d2294c230ae1167f
Nirmal Almara <rkumarnirmal@gmail.com>
2017-02-02 12:06:36 +0530

    design tweak

--- gulpfile.js
+++ gulpfile.js
@@ -2,39 +2,39 @@ var gulp = require('gulp')
 var sass = require('gulp-sass')
 var autoprefixer = require('gulp-autoprefixer')
 
-gulp.task('sass', function () {
-  return gulp.src('app/static/css/_scss/style.scss')
-    .pipe(sass()) // Using gulp-sass
-    .pipe(gulp.dest('app/static/css'))
-})
-
-gulp.task('autoprefixer', function () {
-  return gulp.src('app/static/css/style.css')
-  .pipe(autoprefixer({
-    browsers: ['last 2 versions'],
-    cascade: false
-  }))
-  .pipe(gulp.dest('app/static/css'))
-})
-
-gulp.task('watch', function () {
-  gulp.watch('app/static/css/_scss/*', ['sass'])
-  gulp.watch('app/static/css/*', ['autoprefixer'])
-})
-
 // gulp.task('sass', function () {
-//   return gulp.src('design/static/css/_scss/style.scss')
+//   return gulp.src('app/static/css/_scss/style.scss')
 //     .pipe(sass()) // Using gulp-sass
-//     .pipe(gulp.dest('design/static/css'))
+//     .pipe(gulp.dest('app/static/css'))
 // })
 
 // gulp.task('autoprefixer', function () {
-//   return gulp.src('design/static/css/style.css')
-//   .pipe(autoprefixer())
-//   .pipe(gulp.dest('design/static/css'))
+//   return gulp.src('app/static/css/style.css')
+//   .pipe(autoprefixer({
+//     browsers: ['last 2 versions'],
+//     cascade: false
+//   }))
+//   .pipe(gulp.dest('app/static/css'))
 // })
 
 // gulp.task('watch', function () {
-//   gulp.watch('design/static/css/_scss/*', ['sass'])
-//   gulp.watch('design/static/css/*', ['autoprefixer'])
+//   gulp.watch('app/static/css/_scss/*', ['sass'])
+//   gulp.watch('app/static/css/*', ['autoprefixer'])
 // })
+
+gulp.task('sass', function () {
+  return gulp.src('design/static/css/_scss/style.scss')
+    .pipe(sass()) // Using gulp-sass
+    .pipe(gulp.dest('design/static/css'))
+})
+
+gulp.task('autoprefixer', function () {
+  return gulp.src('design/static/css/style.css')
+  .pipe(autoprefixer())
+  .pipe(gulp.dest('design/static/css'))
+})
+
+gulp.task('watch', function () {
+  gulp.watch('design/static/css/_scss/*', ['sass'])
+  gulp.watch('design/static/css/*', ['autoprefixer'])
+})
