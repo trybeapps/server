fadfd77ad9e1ca298c8b8b09304ba84d637621ba
Nirmal Almara <rkumarnirmal@gmail.com>
2017-05-20 15:08:57 +0530

    Initializing new design

--- gulpfile.js
+++ gulpfile.js
@@ -6,29 +6,29 @@ var minifyCSS = require('gulp-minify-css');
 var concat = require('gulp-concat');
 
 gulp.task('sass', function () {
-  return gulp.src('new_app/static/css/_scss/style.scss')
+  return gulp.src('app/static/css/_scss/style.scss')
     .pipe(sass()) // Using gulp-sass
-    .pipe(gulp.dest('new_app/static/css'))
+    .pipe(gulp.dest('app/static/css'))
 })
 
 gulp.task('autoprefixer', function () {
-  return gulp.src('new_app/static/css/style.css')
+  return gulp.src('app/static/css/style.css')
     .pipe(autoprefixer({
       browsers: ['last 2 versions'],
       cascade: false
     }))
-    .pipe(gulp.dest('new_app/static/css'))
+    .pipe(gulp.dest('app/static/css'))
 })
 
 gulp.task('minifyCSS', function() {
-  return gulp.src('new_app/static/css/style.css')
+  return gulp.src('app/static/css/style.css')
     .pipe(minifyCSS())
     .pipe(rename('style.min.css'))
-    .pipe(gulp.dest('new_app/static/css'))
+    .pipe(gulp.dest('app/static/css'))
 })
 
 gulp.task('watch', function () {
-  gulp.watch('new_app/static/css/_scss/*', ['sass'])
-  gulp.watch('new_app/static/css/*', ['autoprefixer'])
-  gulp.watch('new_app/static/css/*', ['minifyCSS'])
+  gulp.watch('app/static/css/_scss/*', ['sass'])
+  gulp.watch('app/static/css/*', ['autoprefixer'])
+  gulp.watch('app/static/css/*', ['minifyCSS'])
 })
