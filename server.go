13f7c36705fd22bb1377402435123afe4e10bd07
Nirmal Almara <rkumarnirmal@gmail.com>
2017-05-29 23:08:05 +0530

    setting up go

--- server.go
+++ server.go
@@ -9,19 +9,35 @@ import (
 )
 
 func main() {
+
     m := martini.Classic()
     // render html templates from templates directory
   	m.Use(render.Renderer(render.Options{
   		Directory: "templates", // Specify what path to load the templates from.
-  		Layout: "layout", // Specify a layout template. Layouts can call {{ yield }} to render the current template.
+  		Layout: "", // Specify a layout template. Layouts can call {{ yield }} to render the current template.
   		Extensions: []string{".tmpl", ".html"}, // Specify extensions to load for templates.
   		Delims: render.Delims{"{{", "}}"}, // Sets delimiters to the specified strings.
   		Charset: "UTF-8", // Sets encoding for json and html content-types. Default is "UTF-8".
   		IndentJSON: true, // Output human readable JSON
 	}))
 
+	staticOptions := martini.StaticOptions{Prefix: "static"}
+	m.Use(martini.Static("static", staticOptions))
+
   	m.Get("/", func(r render.Render) {
-    	r.HTML(200, "hello", "jeremy")
+    	r.HTML(200, "index", "")
+  	})
+
+  	m.Get("/signin", func(r render.Render) {
+  		r.HTML(200, "signin", "")
+  	})
+
+  	m.Get("/signup", func(r render.Render) {
+  		r.HTML(200, "signup", "")
+  	})
+
+  	m.Get("/collections", func(r render.Render) {
+  		r.HTML(200, "collections", "")
   	})
 
     m.Post("/login", func(r *http.Request) string {
