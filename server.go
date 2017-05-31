3eaee5bc4906538ba69da7609b1cbdea935cfd50
Nirmal Almara <rkumarnirmal@gmail.com>
2017-05-31 22:57:11 +0530

    working on confirmation email

--- server.go
+++ server.go
@@ -2,9 +2,11 @@ package main
 
 import (
     "fmt"
+    "time"
     "database/sql"
     "net/http"
     "runtime"
+    "math/rand"
 
     "github.com/gin-gonic/gin"
     "github.com/gin-contrib/sessions"
@@ -35,14 +37,31 @@ func main() {
     // -------------------------------------------------
     // Fields: id, name, email, password_hash, confirmed
     // -------------------------------------------------
-    stmt, err := db.Prepare("CREATE TABLE IF NOT EXISTS `user` (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `name` VARCHAR(255) NOT NULL, `email` VARCHAR(255) UNIQUE NOT NULL, `password_hash` VARCHAR(255) NOT NULL, `confirmed` INTEGER DEFAULT 0)")
+    stmt, err := db.Prepare("CREATE TABLE IF NOT EXISTS `user` " +
+        "(`id` INTEGER PRIMARY KEY AUTOINCREMENT, `name` VARCHAR(255) NOT NULL," +
+        " `email` VARCHAR(255) UNIQUE NOT NULL, `password_hash` VARCHAR(255) NOT NULL," +
+        " `confirmed` INTEGER DEFAULT 0)")
     CheckError(err)
     
     _, err = stmt.Exec()
     CheckError(err)
 
+    // Create confirm table
+    // Table: confirm
+    // -----------------------------------------------------------------------------------------------------------
+    // Fields: id, token, date_generated, date_expires, date_used, used, user_id (foreign key referencing user id)
+    // -----------------------------------------------------------------------------------------------------------
+    stmt, err = db.Prepare("CREATE TABLE IF NOT EXISTS `confirm` (`id` INTEGER PRIMARY KEY AUTOINCREMENT," +
+        " `token` VARCHAR(255) NOT NULL, `date_generated` VARCHAR(255) NOT NULL," +
+        " `date_expires` VARCHAR(255) NOT NULL, `date_used` VARCHAR(255), " +
+        " `used` INTEGER DEFAULT 0, user_id INTEGER NOT NULL)")
+    CheckError(err)
+
+    _, err = stmt.Exec()
+    CheckError(err)
+
     // Close sqlite3 database
-    db.Close()
+    defer db.Close()
 
     // Router
     r.GET("/", GetHomePage)
@@ -50,9 +69,11 @@ func main() {
     r.POST("/signin", PostSignIn)
     r.GET("/signup", GetSignUp)
     r.POST("/signup", PostSignUp)
+    r.GET("/confirm-email", ConfirmEmail)
+    r.GET("/new-token", SendNewToken)
 
     // Listen and serve on 0.0.0.0:8080
-    r.Run()
+    r.Run(":8080")
 }
 
 func GetHomePage(c *gin.Context) {
@@ -94,6 +115,7 @@ func PostSignIn(c *gin.Context) {
 
     var hashedPassword []byte
     
+    defer rows.Close()
     if rows.Next() {
         err := rows.Scan(&hashedPassword)
         CheckError(err)
@@ -150,30 +172,128 @@ func PostSignUp(c *gin.Context) {
 
     db.Close()
 
-    go SendEmail(email)
+    go SendEmail(id, name, email)
+
+    c.String(http.StatusOK, "We have sent you a confirmation email for verification.")
 
 }
 
-func SendEmail(email string) {
+// For confirm email token
+var letters = []rune("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
-    // Sets home many CPU cores this function want to use.
+func randSeq(n int) string {
+    b := make([]rune, n)
+    for i := range b {
+        b[i] = letters[rand.Intn(len(letters))]
+    }
+    return string(b)
+}
+
+func SendEmail(id int64, name string, email string) {
+
+    // Set home many CPU cores this function wants to use.
     runtime.GOMAXPROCS(runtime.NumCPU())
     fmt.Println(runtime.NumCPU())
+
+    token := randSeq(40)
     
+    t := time.Now()
+    
+    dateGenerated := t.Format("20060102150405")
+    fmt.Println("Token Date Generated: " + dateGenerated)
+    
+    dateExpires := t.AddDate(0,1,0).Format("20060102150405")
+    fmt.Println("Token Date Expires: " + dateExpires)
+
+    userId := id
+
+    db, err := sql.Open("sqlite3", "./libreread.db")
+    CheckError(err)
+
+    stmt, err := db.Prepare("INSERT INTO confirm (token, date_generated, date_expires, user_id) VALUES (?, ?, ?, ?)")
+    CheckError(err)
+
+    _, err = stmt.Exec(token, dateGenerated, dateExpires, userId)
+    CheckError(err)
+
+    db.Close()
+
+    confirmEmailLink := "http://localhost:8080/confirm-email?token=" + token
+
     m := gomail.NewMessage()
-    m.SetHeader("From", "info@libreread.org")
+    m.SetHeader("From", "no-reply@libreread.org")
     m.SetHeader("To", email)
-    // m.SetAddressHeader("Cc", "dan@example.com", "Dan")
-    m.SetHeader("Subject", "Hello!")
-    m.SetBody("text/html", "Hello <b>Bob</b> and <i>Cora</i>!")
+    // m.SetAddressHeader("Cc", "hello@nirm.al", "Nirmal")
+    m.SetHeader("Subject", "LibreRead Email Confirmation")
+    m.SetBody("text/html", "Hi " + name + 
+        ",<br><br>Please confirm your email by clicking this link<br>" + 
+        confirmEmailLink )
     // m.Attach("/home/Alex/lolcat.jpg")
 
-    d := gomail.NewDialer("smtp.zoho.com", 587, "info@libreread.org", "magicmode")
+    d := gomail.NewDialer("smtp.zoho.com", 587, "no-reply@libreread.org", "magicmode")
 
-    // Send the email to Bob, Cora and Dan.
+    // Send the confirmation email
     if err := d.DialAndSend(m); err != nil {
         panic(err)
     }
+}
+
+func ConfirmEmail(c * gin.Context) {
+    token := c.Request.URL.Query()["token"][0]
+    fmt.Println(token)
+
+    db, err := sql.Open("sqlite3", "./libreread.db")
+    CheckError(err)
+
+    defer db.Close()
+
+    // Get id from confirm table with the token got from url.
+    rows, err := db.Query("select id, date_expires, user_id from confirm where token = ?", token)
+    CheckError(err)
+
+    var (
+        id int
+        dateExpires string
+        userId int
+    )
+    
+    if rows.Next() {
+        err := rows.Scan(&id, &dateExpires, &userId)
+        CheckError(err)
+
+        fmt.Println(id)
+        fmt.Println(dateExpires)
+    }
+    rows.Close()
+
+    t := time.Now()
+    if currentDateTime := t.Format("20060102150405"); currentDateTime < dateExpires {
+        stmt, err := db.Prepare("update confirm set date_used=?, used=? where id=?")
+        CheckError(err)
+
+        _, err = stmt.Exec(currentDateTime, 1, id)
+        CheckError(err)
+
+        stmt, err = db.Prepare("update user set confirmed=? where id=?")
+        CheckError(err)
+
+        _, err = stmt.Exec(1, userId)
+
+        // c.String(http.StatusOK, "Thanks! Your email is confirmed successfully. Please signin now.")
+        c.HTML(http.StatusOK, "confirmed.html", gin.H{
+            "id": userId,
+        })
+        return
+    }
+
+    c.HTML(http.StatusOK, "expired.html", gin.H{
+        "id": userId,
+    })
+}
+
+func SendNewToken(c * gin.Context) {
+    userId := c.Request.URL.Query()["id"][0]
+    fmt.Println(userId)
 }
 
 func CheckError(err error) {
