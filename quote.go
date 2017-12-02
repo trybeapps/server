f73395e5453e03b710628c3dde9d88231879f5f7
mysticmode <hello@nirm.al>
2017-12-02 19:50:46 +0530

    quotation update

--- quote.go
+++ quote.go
@@ -35,11 +35,11 @@ func main() {
 
 func GetQuote(c *gin.Context) {
 	c.JSON(200, gin.H{
-		"quote":       "If you want to know what a man's like, take a good look at how he treats his inferiors, not his equals.",
-		"author":      "J.K. Rowling",
-		"authorURL":   "https://www.goodreads.com/author/show/1077326.J_K_Rowling",
-		"image":       "https://images.gr-assets.com/authors/1415945171p2/1077326.jpg",
-		"fromBook":    "Harry Potter and the Goblet of Fire",
-		"fromBookURL": "https://www.goodreads.com/book/show/6.Harry_Potter_and_the_Goblet_of_Fire",
+		"quote":       "Never doubt that a small group of thoughtful, committed, citizens can change the world. Indeed, it is the only thing that ever has.",
+		"author":      "Margaret Mead",
+		"authorURL":   "https://www.goodreads.com/author/show/61107.Margaret_Mead",
+		"image":       "https://images.gr-assets.com/authors/1198589352p2/61107.jpg",
+		"fromBook":    "",
+		"fromBookURL": "",
 	})
 }
